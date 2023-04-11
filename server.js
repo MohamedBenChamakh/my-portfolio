519eccc62ab55b6f91ff2eb84e7637bf51aaf66b
MohamedBenChamakh <mohamed.ben.chamakh98@gmail.com>
2023-04-11 12:40:14 +0100

    fix server.js

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
+const PORT = process.env.PORT || 80;
+app.use(express.static(path.join(__dirname + "/dist/app")));
 
-app.listen(process.env.PORT);
+app.get("/*", (req, res) =>
+  res.sendFile(path.join(__dirname + "/dist/app/index.html"))
+);
+
+console.log("listening on port : " , PORT);
+app.listen(PORT);
